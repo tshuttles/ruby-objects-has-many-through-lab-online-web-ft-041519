aa3ed2aba5c6a28338bdb85d0697733d8a688243
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-08 23:15:33 +0000

    Automatically backed up by Learn

--- spec/04_doctor_spec.rb
+++ spec/04_doctor_spec.rb
@@ -42,7 +42,7 @@ describe 'Doctor' do
     it 'has many patients, through appointments' do
       doctor_who = Doctor.new('The Doctor')
       hevydevy = Patient.new('Devin Townsend')
-      doctor_who.new_appointment(hevydevy, 'Friday, January 32nd', hevydevy)
+      doctor_who.new_appointment('Friday, January 32nd', hevydevy)
 
       expect(doctor_who.patients).to include(hevydevy)
     end

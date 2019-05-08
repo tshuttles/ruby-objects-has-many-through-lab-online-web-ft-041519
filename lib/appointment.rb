d809263858565355ff7df7c0166fb3adf4cba94d
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-08 22:00:10 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,9 +4,10 @@ class Appointment
   
   @@all = [] 
   
-  def initialize(patient, date)
+  def initialize(patient, date, doctor)
     @date = date 
     @patient = patient 
+    @doctor = doctor 
     @@all << self 
   end 
   

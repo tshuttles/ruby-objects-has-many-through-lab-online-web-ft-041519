f2663d574e77e4be794b4caee44c594357765c29
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-08 22:05:59 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -15,7 +15,7 @@ class Doctor
     @@all 
   end 
   
-  def new_appointment(patient, date)
+  def new_appointment(patient, date, )
     a = Appointment.new(patient, date)
     a.doctor = self 
     a 
